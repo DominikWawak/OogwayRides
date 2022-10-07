ed5f1ed92353aac7b45328f988adc2018d15a726
DominikWawak <dominikwawak@gmail.com>
2022-10-07 12:46:31 +0100

    comments and bug fixes

--- src/main/kotlin/org/oogwayrides/console/models/AdventureStore.kt
+++ src/main/kotlin/org/oogwayrides/console/models/AdventureStore.kt
@@ -8,23 +8,16 @@ import org.oogwayrides.console.main.colAdventures
 
 interface AdventureStore {
 
+    /**
+     * Interface, methods for mem store, methods have option to select the collection from mongo allowing for testing.
+     */
     fun addAdventure(id:Int,location:String,date:String,plan:String,vehicle:String,numOfPass:String): Adventure?
-
     fun deleteAdventure(advList: List<Adventure>, input: Int, colAdventures: MongoCollection<Adventure>)
     fun deleteAdventure( colAdventures: MongoCollection<Adventure>, id:Int)
-
-    fun addPassenger(
-        selectedAdv: Adventure,
-        user: User,
-        colAdventures: MongoCollection<Adventure>
-    ): Boolean
-
+    fun addPassenger(selectedAdv: Adventure, user: User, colAdventures: MongoCollection<Adventure>): Boolean
     fun removePassenger(colAdventures: MongoCollection<Adventure>,id: Int,user:User)
-    fun editAdventure(choseAdventure:Adventure,location: String,date: String,plan: String,vehicle: String,numOfPass: String)
-
-
+    fun editAdventure(choseAdventure:Adventure,location: String,date: String,plan: String,vehicle: String,numOfPass: String,colAdventures: MongoCollection<Adventure>)
     fun filterByLocation(advList: List<Adventure>)
-
     fun search(searchLocation:String,list:List<Adventure>): ArrayList<Adventure>
 
 
