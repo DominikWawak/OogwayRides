b5d68acb29c0d3c92567293ef6b25f61254db67d
DominikWawak <dominikwawak@gmail.com>
2022-10-09 21:14:06 +0100

    set up of android app with google maps API, working on getting current location

---
+++ app/src/main/java/com/myapp/oogwayrides_android/splashScreen/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.myapp.oogwayrides_android.splashScreen
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.annotation.RequiresApi
+import com.myapp.oogwayrides_android.MapsActivity
+import com.myapp.oogwayrides_android.R
+
+
+// youtube
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen,MapsActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
